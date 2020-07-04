ee6875efd996fe4325c8be2c02f0a6761d6230d9
marcoslacroix <marcoslacroix@gmail.com>
2020-07-04 16:04:25 -0300

    Department entity class

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,10 @@
+package application.model.entities;
+
+public class Seller {
+
+    private int id;
+    private String name;
+    private String emaill;
+    private Date birthDate;
+    private double salary;
+}
